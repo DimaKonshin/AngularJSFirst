9a2164ef219a697295f2e4ca99950e81557a9aa0
Dima <some@mail>
2018-05-22 23:57:32 +0400

    AppStart кажется лишьним в котроллере

---
+++ WebApplication1/Assets/Scripts/AppStart.js
@@ -0,0 +1,9 @@
+﻿function AppStart($scope) {
+    var p1 = new Point("Hello");
+
+    $scope.message = p1.message;
+
+    $scope.addProduct = function (productName) {
+        $scope.message = productName;
+    }
+}
---
+++ WebApplication1/Assets/Scripts/Point.js
@@ -0,0 +1,14 @@
+﻿function Point(message) {
+    this.message = message;
+
+    var showMessage2 = function () {
+        this.message = "default message";
+        console.log(this.message);
+    }
+
+    showMessage2();
+}
+
+Point.prototype.showMessage = function () {
+    console.log(this.message);
+}
